d2171366bde931b78629653972c27708bca6f152
Vinesh Raju <vineshraju@rocketmail.com>
2020-05-02 12:45:45 +0530

    Adds new theme

--- lib/constants/theme.dart
+++ lib/constants/theme.dart
@@ -3,17 +3,17 @@ import 'package:flutter/material.dart';
 class SiteTheme {
   static ThemeData get theme {
     final themeData = ThemeData.dark();
-
-    return ThemeData.dark().copyWith(
-      primaryColor: Colors.red[800],
-      accentColor: Colors.white,
-      buttonColor: Colors.grey[800],
-      textSelectionColor: Colors.red[100],
-      toggleableActiveColor: Colors.red[300],
-      snackBarTheme: SnackBarThemeData(
-        backgroundColor: themeData.dialogBackgroundColor,
-        actionTextColor: Colors.red[300],
-      ),
+    final textTheme = themeData.primaryTextTheme.copyWith(
+        button: TextStyle(fontSize: 16, color: Color(0xffffffff)),
+        bodyText2: TextStyle(fontSize: 16, color: Color(0xff000000))
+    );
+    return themeData.copyWith(
+        primaryColor: Color(0xffd50000),
+        primaryColorLight: Color(0xffff5131),
+        primaryColorDark: Color(0xff9b0000),
+        accentColor: Color(0xfffafafa),
+        scaffoldBackgroundColor: Color(0xffffffff),
+        textTheme: textTheme
     );
   }
 }
