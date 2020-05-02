8c960e75a5208fd4fb4c1100ee1339a14232eba5
Vinesh Raju <vineshraju@rocketmail.com>
2020-05-02 12:46:46 +0530

    Adds a place holder for logo

--- lib/widgets/navbar/navbar.dart
+++ lib/widgets/navbar/navbar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:thevinesh/page/page.dart';
-import 'package:thevinesh/widgets/navbar/site_sliding_segmented_control.dart';
+import 'package:thevinesh/widgets/widgets.dart';
 
 class NavBar extends StatefulWidget {
   @override
@@ -9,52 +9,59 @@ class NavBar extends StatefulWidget {
 }
 
 class _NavBarState extends State<NavBar> {
+  // ignore: close_sinks
+  PageBloc _bloc;
   SitePage _currentTab;
 
   Map<SitePage, Widget> _createTabs() {
     Map tabsMap = Map<SitePage, Widget>();
     SitePage.values.forEach((tab) {
-      tabsMap.putIfAbsent(tab, () => Text("${tab.toName()}"));
+      tabsMap.putIfAbsent(
+          tab,
+          () => Text(
+                "${tab.toName()}",
+                style: Theme.of(context).textTheme.button,
+              ));
     });
     return tabsMap;
   }
 
   @override
   Widget build(BuildContext context) {
-    // ignore: close_sinks
-    final PageBloc _bloc = BlocProvider.of<PageBloc>(context);
+    _bloc = BlocProvider.of<PageBloc>(context);
     return Container(
       width: double.infinity,
-      color: Theme
-          .of(context)
-          .primaryColor,
+      color: Theme.of(context).primaryColor,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisSize: MainAxisSize.max,
         children: [
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: SizedBox(
-                height: 32,
-                width: 32,
-                child: Placeholder()
-            ),
+          InkWell(
+            onTap: () => _onTabSelected(PageBloc.initialPage),
+            child: Container(
+                padding: EdgeInsets.all(4),
+                height: 36,
+                width: 36,
+                child: Placeholder()),
           ),
-          SlidingSegmentedControlWithThumbUnderneath(
-            thumbColor: Theme
-                .of(context)
-                .accentColor,
-            children: _createTabs(),
-            groupValue: _currentTab ?? _bloc.initialPage,
-            onValueChanged: (value) {
-              setState(() {
-                _currentTab = value;
-                _bloc.add(PageChangeClicked(value));
-              });
-            },
+          Padding(
+            padding: EdgeInsets.all(4),
+            child: SlidingSegmentedControlWithThumbUnderneath(
+              thumbColor: Theme.of(context).primaryTextTheme.bodyText1.color,
+              children: _createTabs(),
+              groupValue: _currentTab ?? PageBloc.initialPage,
+              onValueChanged: (value) {
+                _onTabSelected(value);
+              },
+            ),
           )
         ],
       ),
     );
   }
+
+  void _onTabSelected(SitePage tab) => setState(() {
+        _currentTab = tab;
+        _bloc?.add(PageChangeClicked(tab));
+      });
 }
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,2 @@
+export './navbar/navbar.dart';
+export './navbar/site_sliding_segmented_control.dart';
